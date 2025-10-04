bd8b2d9c542a0d5c8ea1356a3b7fe999740683bd
Dylanjtholen <100318188+dylanjtholen@users.noreply.github.com>
2025-10-04 09:57:14 -0500

    gl init

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-
 import {render, initRenderer} from './renderer';
 import {tick} from './game';
 
 window.addEventListener('DOMContentLoaded', async () => {
 	const canvas = document.getElementById('gameCanvas');
 	await initRenderer(canvas);
-	// You can now call render() as needed
 });
