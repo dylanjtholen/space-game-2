4a9ea3abf1233daacef251818987c3013da409dd
Dylanjtholen <100318188+dylanjtholen@users.noreply.github.com>
2025-10-04 09:56:53 -0500

    gl init

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-import {render} from './renderer';
+
+import {render, initRenderer} from './renderer';
 import {tick} from './game';
+
+window.addEventListener('DOMContentLoaded', async () => {
+	const canvas = document.getElementById('gameCanvas');
+	await initRenderer(canvas);
+	// You can now call render() as needed
+});
--- renderer.js
+++ renderer.js
@@ -1,1 +1,43 @@
-export function render() {}
+let gl;
+let shaderProgram;
+
+async function loadShaderSource(url) {
+	const response = await fetch(url);
+	return await response.text();
+}
+
+function compileShader(gl, source, type) {
+	const shader = gl.createShader(type);
+	gl.shaderSource(shader, source);
+	gl.compileShader(shader);
+	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+		const info = gl.getShaderInfoLog(shader);
+		gl.deleteShader(shader);
+		throw new Error('Could not compile shader: ' + info);
+	}
+	return shader;
+}
+
+export async function initRenderer(canvas) {
+	gl = canvas.getContext('webgl');
+	if (!gl) throw new Error('WebGL not supported');
+
+	const vertSrc = await loadShaderSource('vert.glsl');
+	const fragSrc = await loadShaderSource('frag.glsl');
+
+	const vertShader = compileShader(gl, vertSrc, gl.VERTEX_SHADER);
+	const fragShader = compileShader(gl, fragSrc, gl.FRAGMENT_SHADER);
+
+	shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vertShader);
+	gl.attachShader(shaderProgram, fragShader);
+	gl.linkProgram(shaderProgram);
+	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+		throw new Error('Could not link program: ' + gl.getProgramInfoLog(shaderProgram));
+	}
+	gl.useProgram(shaderProgram);
+}
+
+export function render(scene) {
+	if (!gl) throw new Error('initRenderer has not been called');
+}
