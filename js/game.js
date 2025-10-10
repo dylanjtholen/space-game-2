5426dca74792a38514e1cc87d463043e670c1fc5
Dylanjtholen <100318188+dylanjtholen@users.noreply.github.com>
2025-10-10 13:26:42 -0500

    remove fps controls

--- js/game.js
+++ js/game.js
@@ -35,66 +35,6 @@ export function tick(scene, dt = 1 / 60) {
 		controls(player);
 	}
 
-	/*	
-	fps control mode
-	let moveX = 0;
-	let moveZ = 0;
-	let moveY = 0;
-	
-	if (keys['w']) moveZ += 1;
-	if (keys['s']) moveZ -= 1;
-	if (keys['a']) moveX -= 1;
-	if (keys['d']) moveX += 1;
-	if (keys['shift']) moveY -= 1;
-	if (keys[' ']) moveY += 1;
-	// build movement vector in world space
-	const moveVec = vec3.create();
-	if (moveZ !== 0) {
-		vec3.scaleAndAdd(moveVec, moveVec, forward, moveZ);
-		}
-		if (moveX !== 0) {
-			vec3.scaleAndAdd(moveVec, moveVec, right, moveX);
-			}
-			
-			if (moveY !== 0) {
-				vec3.scaleAndAdd(moveVec, moveVec, up, moveY);
-				}
-				
-				if (vec3.length(moveVec) > 0) {
-					vec3.normalize(moveVec, moveVec);
-					vec3.scale(moveVec, moveVec, moveSpeed * dt);
-					player.position.x += moveVec[0];
-					player.position.y += moveVec[1];
-					player.position.z += moveVec[2];
-					}
-					
-					let yaw = 0;
-					if (keys['arrowleft']) yaw += 1;
-					if (keys['arrowright']) yaw -= 1;
-					if (yaw !== 0) {
-						const angle = yaw * rotSpeed * dt;
-						const q = quat.setAxisAngle(quat.create(), [0, 1, 0], angle);
-						quat.multiply(player.rotation, player.rotation, q);
-						}
-						let pitch = 0;
-						if (keys['arrowup']) pitch += 1;
-						if (keys['arrowdown']) pitch -= 1;
-						if (pitch !== 0) {
-							const angle = pitch * rotSpeed * dt;
-							const right = vec3.transformQuat(vec3.create(), [1, 0, 0], player.rotation);
-							const q = quat.setAxisAngle(quat.create(), right, angle);
-							quat.multiply(player.rotation, q, player.rotation);
-							}
-							let roll = 0;
-							if (keys['q']) roll += 1;
-							if (keys['e']) roll -= 1;
-							if (roll !== 0) {
-								const angle = roll * rotSpeed * dt;
-								const forward = vec3.transformQuat(vec3.create(), [0, 0, -1], player.rotation);
-								const q = quat.setAxisAngle(quat.create(), forward, angle);
-								quat.multiply(player.rotation, q, player.rotation);
-								}*/
-
 	return scene;
 }
 
